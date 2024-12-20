21ba18e745387f167713803757668e6d5216a41e
senkentarou <masahiro-senda@c-fo.com>
2024-12-20 22:26:49 +0900

    main要素をstyledcomponentに置き換え

--- src/app/MainLayout.tsx
+++ src/app/MainLayout.tsx
@@ -1,9 +1,20 @@
 import { Route, Routes } from "react-router-dom";
 import { MdHome, MdRouter } from "react-icons/md";
 import { Footer, GlobalNavi, Header, LinkContent } from "@freee_jp/vibes";
+import styled from "styled-components";
+
 import { Home } from "@screens/Home";
 import { About } from "@screens/About";
 import { NotFound } from "@screens/NotFound";
+
+// With respect to https://vibes.freee.co.jp/?path=/docs/lv1-bases-container--docs
+const StyledMain = styled.main`
+  width: calc(100vw - 3rem);
+  min-width: 20rem;
+  max-width: 100rem;
+  margin: 0 auto;
+  height: calc(100vh - 3rem);
+`;
 
 const links: LinkContent[] = [
   {
@@ -29,7 +40,9 @@ export const MainLayout = () => {
     <>
       <Header logo={<p>senkentarou</p>} sectionNode={null} />
       <GlobalNavi hideHelpForm links={links} />
-      <ScreenRoutes />
+      <StyledMain>
+        <ScreenRoutes />
+      </StyledMain>
       <Footer
         width="wide"
         disableAppStoreBadge
