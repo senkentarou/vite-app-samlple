9d54396ca9354725ee084334f8f2fa9445ae3651
senkentarou <masahiro-senda@c-fo.com>
2024-12-22 07:55:06 +0900

    Aboutページの表現変更

--- src/features/screens/About.tsx
+++ src/features/screens/About.tsx
@@ -1,14 +1,27 @@
-import { CardNavigation, ContentsBase } from "@freee_jp/vibes";
+import {
+  ContentsBase,
+  GridBlock,
+  GridWrapper,
+  ListCard,
+  MaterialIcon,
+  Paragraph,
+} from "@freee_jp/vibes";
 import { MdPerson } from "react-icons/md";
 
 export const About = () => {
-  const navigationContents = [
-    { title: "Me", url: "/about/me", IconComponent: MdPerson },
-  ];
-
   return (
     <ContentsBase>
-      <CardNavigation navigationContents={navigationContents} />
+      <GridWrapper>
+        <GridBlock size="half">
+          <ListCard
+            title="Me"
+            url="/about/me"
+            thumbnail={<MaterialIcon IconComponent={MdPerson} />}
+          >
+            <Paragraph>aaaaaaaaaaaaaaaaaaaaa</Paragraph>
+          </ListCard>
+        </GridBlock>
+      </GridWrapper>
     </ContentsBase>
   );
 };
