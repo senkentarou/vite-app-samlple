9301a3737d4e4269ea5e43a9021e58d22435d19b
senkentarou <masahiro-senda@c-fo.com>
2024-12-21 18:08:58 +0900

    styled-componentのwarning解消

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths"; // tsconfig.paths.jsonの内容をviteに読み込むプラグイン
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      "styled-components": path.resolve(
+        __dirname,
+        "node_modules",
+        "styled-components"
+      ),
+    },
+  },
 });
